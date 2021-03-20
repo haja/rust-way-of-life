c5a776ffcae785a01274e4b9457c4ba0679d25a5
haja <451422+haja@users.noreply.github.com>
2021-03-20 21:08:41 +0100

    use rand crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
 #[derive(Clone)]
 pub struct Game {
-  width: u16,
-  height: u16,
+  cells: Box<Vec<Vec<Cell>>>
 }
 
 impl Game {
-  pub fn new(width: u16, height: u16, seed: i32) -> Game {
+  pub fn new(width: u16, height: u16, seed: u16) -> Game {
+    let cells = Box::new(
+      (0..height)
+          .map(|y| {
+            (0..width)
+                .map(|x| {
+                  let alive = rand::random();
+                  Cell { alive }
+                })
+                .collect()
+          })
+          .collect()
+    );
     Game {
-      width,
-      height,
+      cells
     }
   }
 
@@ -16,20 +26,18 @@ impl Game {
     self.clone()
   }
 
-  pub fn row_columns(&self) -> Vec<Vec<Cell>> {
-    (0..self.height)
-        .map(|y| {
-          (0..self.width)
-              .map(|x| {
-                Cell { alive: false }
-              })
-              .collect()
-        })
-        .collect()
+  pub fn row_columns(&self) -> &Vec<Vec<Cell>> {
+    &self.cells
   }
 }
 
 
+fn prand(seed: u16, y: u16, x: u16) -> u16 {
+  let (uneven, _) = seed.overflowing_shr(1);
+  uneven.wrapping_mul(y + 1).wrapping_mul(x + 1)
+}
+
+#[derive(Clone)]
 pub struct Cell {
   pub alive: bool,
 }
