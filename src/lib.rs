bfdd5c84f8904f3d3cf1fa8360e0a631c0156255
haja <451422+haja@users.noreply.github.com>
2021-03-21 11:56:53 +0100

    Game: use seed for setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
+use rand_xoshiro::Xoshiro256PlusPlus;
+use rand_xoshiro::rand_core::{SeedableRng, RngCore};
+
 #[derive(Clone)]
 pub struct Game {
   cells: Box<Vec<Vec<Cell>>>
 }
 
 impl Game {
-  pub fn new(width: u16, height: u16, seed: u16) -> Game {
+  pub fn new(width: u16, height: u16, seed: u64) -> Game {
+    let mut rng = Xoshiro256PlusPlus::seed_from_u64(seed);
     let cells = Box::new(
       (0..height)
           .map(|y| {
             (0..width)
                 .map(|x| {
-                  let alive = rand::random();
+                  let alive = rng.next_u32() % 2 == 0;
                   Cell { alive }
                 })
                 .collect()
@@ -29,12 +33,6 @@ impl Game {
   pub fn row_columns(&self) -> &Vec<Vec<Cell>> {
     &self.cells
   }
-}
-
-
-fn prand(seed: u16, y: u16, x: u16) -> u16 {
-  let (uneven, _) = seed.overflowing_shr(1);
-  uneven.wrapping_mul(y + 1).wrapping_mul(x + 1)
 }
 
 #[derive(Clone)]
