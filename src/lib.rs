5b90e47f7ded29842443e2be0180126eb2627c1a
haja <451422+haja@users.noreply.github.com>
2021-03-21 12:05:51 +0100

    add basic tests, fix warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
+use rand_xoshiro::rand_core::{RngCore, SeedableRng};
 use rand_xoshiro::Xoshiro256PlusPlus;
-use rand_xoshiro::rand_core::{SeedableRng, RngCore};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Game {
   cells: Box<Vec<Vec<Cell>>>
+}
+
+#[derive(Clone, Debug)]
+pub struct Cell {
+  pub alive: bool,
 }
 
 impl Game {
@@ -11,9 +16,9 @@ impl Game {
     let mut rng = Xoshiro256PlusPlus::seed_from_u64(seed);
     let cells = Box::new(
       (0..height)
-          .map(|y| {
+          .map(|_y| {
             (0..width)
-                .map(|x| {
+                .map(|_x| {
                   let alive = rng.next_u32() % 2 == 0;
                   Cell { alive }
                 })
@@ -35,7 +40,28 @@ impl Game {
   }
 }
 
-#[derive(Clone)]
-pub struct Cell {
-  pub alive: bool,
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn new_game_should_contain_one_row() {
+    let game = Game::new(2, 1, 1);
+
+    assert_eq!(game.row_columns().len(), 1);
+  }
+
+  #[test]
+  fn new_game_should_contain_one_column() {
+    let game = Game::new(1, 2, 1);
+
+    assert_eq!(game.row_columns()[0].len(), 1);
+  }
+
+  #[test]
+  fn new_game_all_columns_should_be_same_length() {
+    let game = Game::new(1, 2, 1);
+
+    assert_eq!(game.row_columns()[0].len(), game.row_columns()[1].len());
+  }
 }
